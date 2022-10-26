b69a53412540de1d1b667f3f09202ff12def7f6d
SinyakinAS <endermix@yandex.ru>
2022-10-26 20:07:48 +0300

    fix

--- modules/andrich_maria_sort_facility/src/sort_facility_app.cpp
+++ modules/andrich_maria_sort_facility/src/sort_facility_app.cpp
@@ -1,6 +1,6 @@
 // Copyright 2022 Sinyakin Andrey
 
-#include "include/simpson_app.h"
+#include "include/sort_facility_app.h"
 
 #include <iostream>
 #include <sstream>
@@ -25,50 +25,54 @@ std::string CLPApplication::operator()(int argc, const char** argv) {
   int i = 1;
 
   if (std::string(argv[i]) == "--test1" || std::string(argv[i]) == "-t1") {
-    function<double(vector<double>)> test_func = [](vector<double> vec) {
-      double x = vec[0];
-      return x;
-    };
-    vector<int> test_n = {atoi(argv[2])};
-    vector<pair<double, double>> test_limits = {{atoi(argv[3]), atoi(argv[4])}};
-    SimpsonSolver a(1, test_func, test_limits, test_n);
-    os << a.getDim();
+    Sort s;
+    const char* input = argv[2];
+    char output[128];
+
+    s.shunting_yard(input, output);
+
+    const char* answer = output;
+
+    if (*answer == *input) os << "true";
     std::cout << os.str() << std::endl;
     return "success";
   }
   if (std::string(argv[i]) == "--test2" || std::string(argv[i]) == "-t2") {
-    function<double(vector<double>)> test_func = [](vector<double> vec) {
-      double x = vec[0];
-      return x;
-    };
-    vector<int> test_n = {atoi(argv[2])};
-    vector<pair<double, double>> test_limits = {{atoi(argv[3]), atoi(argv[4])}};
-    SimpsonSolver a(1, test_func, test_limits, test_n);
-    os << a.getN()[0];
+    Sort s;
+    const char* input = argv[2];
+    char output[128];
+
+    s.shunting_yard(input, output);
+
+    const char* answer = output;
+
+    if (*answer == *input) os << "true";
     std::cout << os.str() << std::endl;
     return "success";
   }
   if (std::string(argv[i]) == "--test3" || std::string(argv[i]) == "-t3") {
-    function<double(vector<double>)> test_func = [](vector<double> vec) {
-      double x = vec[0];
-      return x;
-    };
-    vector<int> test_n = {atoi(argv[2])};
-    vector<pair<double, double>> test_limits = {{atoi(argv[3]), atoi(argv[4])}};
-    SimpsonSolver a(1, test_func, test_limits, test_n);
-    os << a.getLimits()[0].first;
+    Sort s;
+    const char* input = argv[2];
+    char output[128];
+
+    s.shunting_yard(input, output);
+
+    const char* answer = output;
+
+    if (*answer == *input) os << "true";
     std::cout << os.str() << std::endl;
     return "success";
   }
   if (std::string(argv[i]) == "--test4" || std::string(argv[i]) == "-t4") {
-    function<double(vector<double>)> test_func = [](vector<double> vec) {
-      double x = vec[0];
-      return x;
-    };
-    vector<int> test_n = {atoi(argv[2])};
-    vector<pair<double, double>> test_limits = {{atoi(argv[3]), atoi(argv[4])}};
-    SimpsonSolver a(1, test_func, test_limits, test_n);
-    os << a.getLimits()[0].second;
+    Sort s;
+    const char* input = argv[2];
+    char output[128];
+
+    s.shunting_yard(input, output);
+
+    const char* answer = output;
+
+    if (*answer == *input) os << "true";
     std::cout << os.str() << std::endl;
     return "success";
   }
